cb490ebc49974679354918f00236c2282f04f185
JoanChirinos <jchirinos@stuy.edu>
2018-05-10 14:05:54 -0400

    fixed add

--- v2/ArrayPriorityQueue.java
+++ v2/ArrayPriorityQueue.java
@@ -16,31 +16,30 @@ public class ArrayPriorityQueue implements PriorityQueue {
   }
 
 
-  //sorts queue based on priority upon adding, sort of like insertion sort
-  //O(n)
+  //sorts queue based on priority upon adding, sort of like binary search
+  //O(logn)
   public void add(String toAdd) {
-
-    //if no elements, or least priority element is higher than toAdd, add to end
-    if (size() == 0 || _data.get(size() - 1).compareTo(toAdd) >= 0) {
+    if (size() == 0 || _data.get(size() - 1).compareTo(toAdd) < 0) {
       _data.add(toAdd);
-      return;
     }
+    else _data.add(whereToAdd(toAdd), toAdd);
+  }
 
-    //else if first element has lower priority than toAdd, add to index 0
-    else if (_data.get(0).compareTo(toAdd) < 0) {
-      _data.add(0, toAdd);
-      return;
-    }
-
-    //otherwise traverse AL and put toAdd into right spot
-    else {
-      for (int pos = size() - 1; pos > 0; pos--) {
-        if (_data.get(pos - 1).compareTo(toAdd) > 0) {
-          _data.add(pos, toAdd);
-          return;
-        }
+  private int whereToAdd(String toAdd) {
+    int left = 0;
+    int right = size();
+    int mid = (left + right) / 2;
+    while (left < right) {
+      mid = (left + right) / 2;
+      if (_data.get(mid).compareTo(toAdd) < 0 && _data.get(mid + 1).compareTo(toAdd) > 0) {
+        return mid + 1;
       }
+      if (_data.get(mid).compareTo(toAdd) < 0) {
+        left = mid + 1;
+      }
+      else right = mid;
     }
+    return mid;
   }
 
   //O(1)
@@ -76,17 +75,23 @@ public class ArrayPriorityQueue implements PriorityQueue {
     ArrayPriorityQueue apq = new ArrayPriorityQueue();
     System.out.println("apq: " + apq);
     apq.add("hoo");
+    System.out.println("Adding hoo");
     System.out.println("apq: " + apq);
     apq.add("boo");
+    System.out.println("Adding boo");
     System.out.println("apq: " + apq);
     apq.add("doo");
+    System.out.println("Adding doo");
     System.out.println("apq: " + apq);
     System.out.println("removing min: " + apq.peekMin());
     apq.removeMin();
     System.out.println("apq: " + apq);
     apq.add("zoo");
+    System.out.println("Adding zoo");
     System.out.println("apq: " + apq);
     apq.add("aoo");
+    System.out.println("Adding aoo");
+    System.out.println("apq: " + apq);
   }
 
 }//end class
