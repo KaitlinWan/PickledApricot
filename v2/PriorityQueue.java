83227d193f6b92570ab74e28a118e7dd16feb297
JoanChirinos <jchirinos@stuy.edu>
2018-05-10 13:48:07 -0400

    Added v2

---
+++ v2/PriorityQueue.java
@@ -0,0 +1,18 @@
+/*
+  Joan Chirinos, Shruiti Venkata
+  HW46 -- Arrr, There Be Priorities Here Matey
+  APCS2 pd08
+  2018-9-18
+*/
+
+public interface PriorityQueue {
+
+  public void add(String toAdd);
+
+  public boolean isEmpty();
+
+  public String peekMin();
+
+  public String removeMin();
+
+}
